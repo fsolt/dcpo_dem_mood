d0ed69098257f817b61a44a61b5755124250b44a
Frederick Solt <frederick-solt@uiowa.edu>
2020-06-25 18:22:08 -0500

    use data/claassen_input.Rda; this converged on argon in 1h:36m:20s  👀@Tyhcass

--- R/argon/dcpo_demsupport/R/claassen_m5.R
+++ R/argon/dcpo_demsupport/R/claassen_m5.R
@@ -1,31 +1,23 @@
-# remotes::install_github("fsolt/DCPOtools")
-# remotes::install_github("stan-dev/rstantools")
-# remotes::install_github("fsolt/DCPO")
-
 library(tidyverse)
-library(DCPOtools)
 library(rstan)
 
-rstan_options(auto_write = TRUE)
+download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/claassen_input.Rda", "data/claassen_input.Rda")
 
-demsup <-
-    read_csv("data/claassen_replication.csv", col_types = "dcdcddcdd")
+load("data/claassen_input.Rda")
 
-demsup_claassen <- format_claassen(demsup)
-
-claassen_m5 <- stan(file = "R/argon/dcpo_demsupport/R/supdem.stan.mod5.stan",
-                       data = demsup_claassen,
-                       iter = 2000,
-                       chains= 4,
-                       cores = 4,
-		       thin = 2,
-                       pars = c("mu_lambda","sigma_lambda","sigma_delta","sigma_theta","phi","lambda","delta","theta",
-            "x_pred","log_lik"),
-                       control = list(adapt_delta=0.99, stepsize=0.02, max_treedepth=11))
+claassen_m5 <- stan(file = 'R/supdem.stan.mod5.stan',
+                    data = claassen_input,
+                    iter = 2000,
+                    chains= 4,
+                    cores = 4,
+                    thin = 2,
+                    pars = c("mu_lambda","sigma_lambda","sigma_delta","sigma_theta","phi","lambda","delta","theta",
+                             "x_pred","log_lik"),
+                    control = list(adapt_delta=0.99, stepsize=0.02, max_treedepth=11))
 
 save(demsup_claassen, claassen_m5, 
      file = str_c("data/claassen_m5_2k_", 
                   str_replace_all(Sys.time(), "[ :]", "-") %>%
-                    str_replace("2019-", "") %>%
-                    str_replace("-\\d{2}$", ""),
+                      str_replace("20\\d{2}-", "") %>%
+                      str_replace("-\\d{2}$", ""),
                   ".rda"))
