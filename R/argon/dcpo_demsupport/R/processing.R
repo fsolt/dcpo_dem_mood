27bf7553edf6e7a3b10f0e726fbba0edb883cd6f
Frederick Solt <frederick-solt@uiowa.edu>
2020-06-26 15:03:29 -0500

    install DCPO from CRAN; download data/dcpo_replication_input.rda

--- R/argon/dcpo_demsupport/R/processing.R
+++ R/argon/dcpo_demsupport/R/processing.R
@@ -1,25 +1,21 @@
-remotes::install_github("fsolt/DCPOtools")
-remotes::install_github("fsolt/DCPO")
+install.packages("DCPO", repos = "https://cloud.r-project.org/")
 
 library(tidyverse)
-library(DCPOtools)
 library(DCPO)
 
-demsup <- read_csv("https://github.com/fsolt/DCPOtools/raw/master/inst/extdata/all_data_demsupport.csv")
+download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/dcpo_replication_input.rda", "data/dcpo_replication_input.rda")
 
-demsup_data <- format_dcpo(demsup %>% with_min_yrs(2),
-                       scale_q = "church_21",
-                       scale_cp = 2)
+load("data/dcpo_replication_input.rda")
 
-out1 <- dcpo(demsup_data,
-                 iter = 2000,
-		 chains = 4,
-		 thin = 2,
-                 pars = c("sd_delta","sd_theta_evolve", "sd_sigma_evolve", "sigma","phi","beta","alpha","delta","theta","y_r_pred","log_lik"))
+dcpo_replication <- dcpo(dcpo_replication_input,
+                         iter = 2000,
+                         chains = 4,
+                         thin = 2,
+                         pars = c("sd_delta","sd_theta_evolve", "sd_sigma_evolve", "sigma","phi","beta","alpha","delta","theta","y_r_pred","log_lik"))
 
-save(demsup_data, out1,
-     file = str_c("data/church_21_2k_",
+save(dcpo_replication_input, dcpo_replication,
+     file = str_c("data/church_lb2_2k_",
                   str_replace_all(Sys.time(), "[ :]", "-") %>%
-                    str_replace("2019-", "") %>%
-                    str_replace("-\\d{2}$", ""),
+                      str_replace("20\\d{2}-", "") %>%
+                      str_replace("-\\d{2}$", ""),
                   ".rda"))
