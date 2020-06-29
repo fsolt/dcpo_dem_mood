c93103d0a2e966d2256ab83852a369979c508493
Frederick Solt <frederick-solt@uiowa.edu>
2020-06-29 11:15:26 -0500

    set iter to 3000 and thin to 3

--- R/argon/dcpo_demsupport/R/claassen_m5.R
+++ R/argon/dcpo_demsupport/R/claassen_m5.R
@@ -7,16 +7,16 @@ load("data/claassen_input.rda")
 
 claassen_m5 <- stan(file = 'R/supdem.stan.mod5.stan',
                     data = claassen_input,
-                    iter = 2000,
+                    iter = 3000,
                     chains= 4,
                     cores = 4,
-                    thin = 2,
+                    thin = 3,
                     pars = c("mu_lambda","sigma_lambda","sigma_delta","sigma_theta","phi","lambda","delta","theta",
                              "x_pred","log_lik"),
                     control = list(adapt_delta=0.99, stepsize=0.02, max_treedepth=11))
 
-save(claassen_input, claassen_m5, 
-     file = str_c("data/claassen_m5_2k_", 
+save(claassen_input, claassen_m5,
+     file = str_c("data/claassen_m5_3k_",
                   str_replace_all(Sys.time(), "[ :]", "-") %>%
                       str_replace("20\\d{2}-", "") %>%
                       str_replace("-\\d{2}$", ""),
--- R/argon/dcpo_demsupport/R/processing.R
+++ R/argon/dcpo_demsupport/R/processing.R
@@ -8,13 +8,13 @@ download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/dcpo_repli
 load("data/dcpo_replication_input.rda")
 
 dcpo_replication <- dcpo(dcpo_replication_input,
-                         iter = 2000,
+                         iter = 3000,
                          chains = 4,
-                         thin = 2,
+                         thin = 3,
                          pars = c("sd_delta","sd_theta_evolve", "sd_sigma_evolve", "sigma","phi","beta","alpha","delta","theta","y_r_pred","log_lik"))
 
 save(dcpo_replication_input, dcpo_replication,
-     file = str_c("data/church_lb2_2k_",
+     file = str_c("data/church_lb2_3k_",
                   str_replace_all(Sys.time(), "[ :]", "-") %>%
                       str_replace("20\\d{2}-", "") %>%
                       str_replace("-\\d{2}$", ""),
