750cdb3f4ae12b53d06754b678dfc305230eb520
Cassandra Tai <yuehongtai@gmail.com>
2020-06-29 13:20:17 -0500

    input claassen_input.rda

--- R/claassen_kfold.R
+++ R/claassen_kfold.R
@@ -6,23 +6,20 @@ source("R/claassen_xvt.R")
 
 rstan_options(auto_write = TRUE)
 
-demsup <-
-    read_csv("data/claassen_replication.csv", col_types = "dcdcddcdd")
+download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/claassen_input.rda", "data/claassen_input.rda")
 
-demsup_data <- DCPOtools::format_claassen(demsup %>%
-                                              DCPOtools::with_min_yrs(3))
-
+load("data/claassen_input.rda")
 
 claassen_demsup_kfold <- purrr::map(1:10, function(x) {
-    claassen_xvt(demsup_data,
-                 fold_number = 10, # number_of_folds = 10 is the default，
-                 iter = 500)
+  claassen_xvt(claassen_input,
+               fold_number = 10, # number_of_folds = 10 is the default,
+               iter = 500)
 })
 
 xvt_claassen_kfold <-
-    get_claassen_xvt_results(claassen_demsup_kfold)
+  get_claassen_xvt_results(claassen_demsup_kfold)
 
 save(
-    list = ls() %>% str_subset("^(?!demsup)"),
-    file = file.path("data", "kfold", "claassen_demsup_kfold.rda")
+  list = ls() %>% str_subset("^(?!demsup)"),
+  file = file.path("data", "kfold", "claassen_demsup_kfold.rda")
 )
