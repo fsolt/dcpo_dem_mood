8f0edb25abc759020d66744248ca509e0df105ef
Frederick Solt <frederick-solt@uiowa.edu>
2020-07-02 19:26:28 -0500

    update

--- R/argon/dcpo_demsupport/R/processing.R
+++ R/argon/dcpo_demsupport/R/processing.R
@@ -3,17 +3,17 @@ install.packages("DCPO", repos = "https://cloud.r-project.org/")
 library(tidyverse)
 library(DCPO)
 
-download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/dcpo_replication_input.rda", "data/dcpo_replication_input.rda")
+download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/dcpo_input.rda", "data/dcpo_input.rda")
 
-load("data/dcpo_replication_input.rda")
+load("data/dcpo_input.rda")
 
-dcpo_replication <- dcpo(dcpo_replication_input,
-                         iter = 3000,
-                         chains = 4,
-                         thin = 3,
-                         pars = c("sd_delta","sd_theta_evolve", "sd_sigma_evolve", "sigma","phi","beta","alpha","delta","theta","y_r_pred","log_lik"))
+dcpo_output <- dcpo(dcpo_input,
+                    iter = 2000,
+                    chains = 4,
+                    thin = 4,
+                    pars = c("sd_delta","sd_theta_evolve", "sd_sigma_evolve", "sigma","phi","beta","alpha","delta","theta","y_r_pred","log_lik"))
 
-save(dcpo_replication_input, dcpo_replication,
+save(dcpo_input, dcpo_output,
      file = str_c("data/church_lb2_3k_",
                   str_replace_all(Sys.time(), "[ :]", "-") %>%
                       str_replace("20\\d{2}-", "") %>%
