453aedec99ebbebbcb53d4eba017df247c7f5b38
Frederick Solt <frederick-solt@uiowa.edu>
2020-07-04 09:33:42 -0500

    use object to set iter; use iter to set thin and filename

--- R/argon/dcpo_demsupport/R/processing.R
+++ R/argon/dcpo_demsupport/R/processing.R
@@ -6,14 +6,16 @@ download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/dcpo_input
 
 load("data/dcpo_input.rda")
 
+iter <- 3000
+
 dcpo_output <- dcpo(dcpo_input,
-                    iter = 2000,
+                    iter = iter,
                     chains = 4,
-                    thin = 4,
+                    thin = iter/500, # this yields 250 draws per chain, 1000 draws total
                     pars = c("sd_delta","sd_theta_evolve", "sd_sigma_evolve", "sigma","phi","beta","alpha","delta","theta","y_r_pred","log_lik"))
 
 save(dcpo_input, dcpo_output,
-     file = str_c("data/church_lb2_3k_",
+     file = str_c("data/church_lb2_", round(iter/1000), "k_",
                   str_replace_all(Sys.time(), "[ :]", "-") %>%
                       str_replace("20\\d{2}-", "") %>%
                       str_replace("-\\d{2}$", ""),
