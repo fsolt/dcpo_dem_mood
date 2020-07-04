1a102ed50f11d62ebdb3f4480dea33a65c7a83b8
Frederick Solt <frederick-solt@uiowa.edu>
2020-07-04 10:18:37 -0500

    rename R/argon/dcpo_demsupport_kfold/*

--- R/argon/dcpo_demsupport_kfold/R/processing.R
+++ R/argon/dcpo_demsupport_kfold/R/processing.R
@@ -1,19 +1,17 @@
-install.packages("DCPO", repos = "https://cloud.r-project.org/")
-
 library(tidyverse)
-library(DCPO)
+if (!require("DCPO")) install.packages("DCPO", repos = "https://cloud.r-project.org/"); library(DCPO)
 
 fold <- commandArgs(trailingOnly=TRUE) %>%
     as.numeric()
 
 print(fold)
 
-download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/dcpo_replication_input.rda", "data/dcpo_replication_input.rda")
+download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/dcpo_input.rda", "data/dcpo_input.rda")
 
-load("data/dcpo_replication_input.rda")
+load("data/dcpo_input.rda")
 
 
-dcpo_kfold <- dcpo_xvt(dcpo_replication_input,
+dcpo_kfold <- dcpo_xvt(dcpo_input,
                        fold_number = fold, 
                        number_of_folds = 10,
                        iter = 2000)
