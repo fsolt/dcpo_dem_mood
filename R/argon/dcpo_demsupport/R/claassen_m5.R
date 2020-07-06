b227fd5c9b5f45c28fdad79773a12ca2a7c9fd70
Frederick Solt <frederick-solt@uiowa.edu>
2020-07-06 09:18:48 -0500

    use object to set iter

--- R/argon/dcpo_demsupport/R/claassen_m5.R
+++ R/argon/dcpo_demsupport/R/claassen_m5.R
@@ -5,18 +5,20 @@ download.file("https://github.com/fsolt/dcpo_dem_mood/raw/master/data/claassen_i
 
 load("data/claassen_input.rda")
 
+iter <- 3000
+
 claassen_m5 <- stan(file = 'R/supdem.stan.mod5.stan',
                     data = claassen_input,
-                    iter = 3000,
+                    iter = iter,
                     chains= 4,
                     cores = 4,
-                    thin = 3,
+                    thin = iter/500,
                     pars = c("mu_lambda","sigma_lambda","sigma_delta","sigma_theta","phi","lambda","delta","theta",
                              "x_pred","log_lik"),
                     control = list(adapt_delta=0.99, stepsize=0.02, max_treedepth=11))
 
 save(claassen_input, claassen_m5,
-     file = str_c("data/claassen_m5_3k_",
+     file = str_c("data/claassen_m5_", round(iter/1000), "k_",
                   str_replace_all(Sys.time(), "[ :]", "-") %>%
                       str_replace("20\\d{2}-", "") %>%
                       str_replace("-\\d{2}$", ""),
