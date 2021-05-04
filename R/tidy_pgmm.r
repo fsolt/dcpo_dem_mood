2196670a0f7ee3bd07acbd3d5c2b8d71f5f1f203
Yue Hu <sammo3182@sina.com>
2021-05-04 15:10:29 +0800

    writing a customized version of glance for plm

--- R/tidy_pgmm.r
+++ R/tidy_pgmm.r
@@ -40,8 +40,26 @@ glance.pgmm <- function(x, ...) {
         # arellano.bond1 = unname(x$m1$statistic[1,]),
         # arellano.bond2 = unname(x$m2$statistic[1,]),
         nobs = stats::nobs(x),
-        # n.country = length(x$residuals),
-        n.inst = dim(x$W[[1]])[2],
+        n.country = length(x$residuals),
+        # n.inst = dim(x$W[[1]])[2],
+        na_types = "ii"
+    )
+}
+
+glance.plm <- function(x, ...) {
+    s <- summary(x)
+    as_glance_tibble(
+        # chisq.sargan = unname(s$sargan$statistic),
+        # df.sargan = unname(s$sargan$parameter),
+        # p.sargan = unname(s$sargan$p.value),
+        # chisq.wald = unname(s$wald.coef$statistic),
+        # df.wald = unname(s$wald.coef$parameter),
+        # p.wald = unname(s$wald.coef$p.value),
+        # arellano.bond1 = unname(x$m1$statistic[1,]),
+        # arellano.bond2 = unname(x$m2$statistic[1,]),
+        nobs = stats::nobs(x),
+        n.country = attr(x$residuals, "index")$Country %>% nlevels,
+        # n.inst = dim(x$W[[1]])[2],
         na_types = "ii"
     )
 }
