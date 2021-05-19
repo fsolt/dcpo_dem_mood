d0349d82cd4bbaf914eb3f1cb103dca88c224ab1
Frederick Solt <frederick-solt@uiowa.edu>
2021-05-19 11:33:38 -0500

    add methodComposition3 as minimal modification

--- R/moc_function.R
+++ R/moc_function.R
@@ -52,3 +52,30 @@ methodComposition2 <- function (data, model, vcov=plmHC) {
   names(coefdf) <- names(coef(model))
   return(coefdf)
 }
+
+
+methodComposition3 <- function (data, iter, model, vcov=plmHC, rsq=TRUE) {
+  coefdf <- as.data.frame(matrix(nrow=length(iter), ncol=length(coef(model))))
+  R2 <- data.frame(rsq=rep(NA, length(iter)), adjrsq=rep(NA, length(iter)))
+  for (s in seq_along(iter)) {
+    ## (1) Sample from p(x)
+    data_sample <- data[[s]]
+    ## (2) Sample from p(B|x,y):
+    ##     (a) Estimate B_s and Cov(B_s) conditional on x_s.
+    mod_sample <- update(model, data=data_sample)
+    hatB_sample <- coef(mod_sample)
+    hatV_sample <- vcov(mod_sample)
+    ##     (b) Sample \data_sample{B_s} from MV(\hat{B_sample}, \hat{Cov(B_sample)}).
+    coefdf[s, ] <- MASS::mvrnorm(n = 1, mu = hatB_sample, Sigma = hatV_sample)
+    if (rsq) {
+      R2$rsq[s] <- summary(mod_sample)$r.squared["rsq"]
+      R2$adjrsq[s] <- summary(mod_sample)$r.squared["adjrsq"]
+    }
+  }
+  names(coefdf) <- names(coef(model))
+  if (rsq) {
+    coefdf$rsq <- R2$rsq
+    coefdf$adjrsq <- R2$adjrsq
+  }
+  return(coefdf)
+}
