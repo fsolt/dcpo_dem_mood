d899c24dfad3fe95705fa91e3c1789857673007d
Tyhcass <yuehongtai@gmail.com>
2021-06-06 19:35:30 -0500

    use here

--- R/cls_ajps_apsr_uncertainty.R
+++ R/cls_ajps_apsr_uncertainty.R
@@ -8,10 +8,10 @@ set.seed(313)
 ######## Create lists for elements in analysis data  ###########
 ########### AJPS: cls_libdem_ajps_list,cls_ajps  ###############
 ################################################################
-load("data/cls_ajps_cntrl.rda")  #cls ajps variables without errors. 
-load("data/cls_theta_list.rda")  #cls's theta list
-load("data/cls_libdem_list.rda")  #vdem8 libdem list
-load("data/country_regionUN.rda") 
+load(here("data","cls_ajps_cntrl.rda"))  #cls ajps variables without errors. 
+load(here("data","cls_theta_list.rda"))  #cls's theta list
+load(here("data","cls_libdem_list.rda"))  #vdem8 libdem list
+load(here("data","country_regionUN.rda"))
 
 libdem_ajps_list <- cls_libdem_list %>%
     map(~.x %>%
@@ -34,7 +34,7 @@ libdem_ajps_list <- cls_libdem_list %>%
             mutate(Libdem_regUN_m1 = lag(Libdem_regUN))
     ) 
 
-saveRDS(libdem_ajps_list, "data/cls_libdem_ajps_list.rds")
+saveRDS(libdem_ajps_list, file = here("data","cls_libdem_ajps_list.rds"))
 #include vdem8 libdem related variables such Libdem, Region_libdem, ChDem
 
 cls_ajps_cntrl_list <-  purrr::map(1:900, function(anEntry) {
@@ -56,19 +56,19 @@ cls_ajps <- purrr::map(1:900, function(anEntry) {
 
 #merge with cls_theta and create trim variables. 
 
-save(cls_ajps, file = "data/cls_ajps.rda")
+save(cls_ajps, file = here("data","cls_ajps.rda"))
 
 ################################################################
 ######## Create lists for elements in analysis data  ###########
 ########### APSR: cls_libdem_ajps_list,cls_ajps  ###############
 ################################################################
-#load("data/cls_ajps_cntrl.rda")
-#load("data/cls_theta_list.rda")
-#load("data/cls_libdem_list.rda")
-load("data/cls_apsr_cntrl.rda")
-load("data/cls_poly_list.rda")
-load("data/cls_liberal_list.rda")
-load("data/cpi_list.rda")
+#load(here("data","cls_ajps_cntrl.rda"))
+#load(here("data","cls_theta_list.rda"))
+#load(here("data","cls_libdem_list.rda"))
+load(here("data","cls_apsr_cntrl.rda"))
+load(here("data","cls_poly_list.rda"))
+load(here("data","cls_liberal_list.rda"))
+load(here("data","cpi_list.rda"))
 
 ## Standardize cls_libdem_list Libdem_z
 cls_libdem_z <- cls_libdem_list %>%
@@ -128,4 +128,4 @@ cls_apsr <- purrr::map(1:900, function(anEntry) {
         select(country, year, First_yr,theta, SupDem_trim,contains("z"),everything())
 }) 
 
-save(cls_apsr, file = "data/cls_apsr.rda")
+save(cls_apsr, file = here("data","cls_apsr.rda"))
